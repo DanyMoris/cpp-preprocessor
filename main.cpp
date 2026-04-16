5bd7201349463dbc4f830867cb27389628a69762
Danil Efremov <danymoris2007@gmail.com>
2026-04-16 15:34:29 +0500

    set IDE for inserting spaces instead of tabs

--- main.cpp
+++ main.cpp
@@ -14,143 +14,140 @@ static const regex QUOTE_RE(R"(\s*#\s*include\s*\"([^\"]*)\"\s*)");
 static const regex ANGLE_RE(R"(\s*#\s*include\s*<([^>]*)>\s*)");
 
 path operator""_p(const char* data, std::size_t sz) {
-	return path(data, data + sz);
+  return path(data, data + sz);
 }
 
-void PrintErrorMessage(const string& filename, const path& in_file, int line_number) {
-	cout << "unknown include file " << filename << " at file " << in_file.string() << " at line " << line_number << endl;
-}
-
+// напишите эту функцию
 bool PreprocessRecursive(const path& in_file, ostream& out, const vector<path>& include_directories) {
-	ifstream in(in_file);
-	if (!in.is_open()) return false;
+  ifstream in(in_file);
+  if (!in.is_open()) return false;
 
-	string line;
-	int line_number = 0;
-	while (getline(in, line)) {
-		++line_number;
-		smatch match;
-		bool found = false;
-		path included_path;
+  string line;
+  int line_number = 0;
+  while (getline(in, line)) {
+	  ++line_number;
+	  smatch match;
+	  bool found = false;
+	  path included_path;
 
-		if (regex_match(line, match, QUOTE_RE)) {
-			string filename = match[1];
-			path parent_dir = in_file.parent_path();
-			if (filesystem::exists(parent_dir / filename)) {
-				included_path = parent_dir / filename;
-				found = true;
-			}
-			else {
-				for (const auto& dir : include_directories) {
-					if (filesystem::exists(dir / filename)) {
-						included_path = dir / filename;
-						found = true;
-						break;
-					}
-				}
-			}
+	  if (regex_match(line, match, QUOTE_RE)) {
+		  string filename = match[1];
+		  path parent_dir = in_file.parent_path();
+		  if (filesystem::exists(parent_dir / filename)) {
+			  included_path = parent_dir / filename;
+			  found = true;
+		  }
+		  else {
+			  for (const auto& dir : include_directories) {
+				  if (filesystem::exists(dir / filename)) {
+					  included_path = dir / filename;
+					  found = true;
+					  break;
+				  }
+			  }
+		  }
 
-			if (found) {
-				if (!PreprocessRecursive(included_path, out, include_directories)) {
-					return false;
-				}
-			}
-			else {
-				PrintErrorMessage(filename, in_file, line_number);
-				return false;
-			}
-		}
-		else if (regex_match(line, match, ANGLE_RE)) {
-			string filename = match[1];
-			for (const auto& dir : include_directories) {
-				if (filesystem::exists(dir / filename)) {
-					included_path = dir / filename;
-					found = true;
-					break;
-				}
-			}
+		  if (found) {
+			  if (!PreprocessRecursive(included_path, out, include_directories)) {
+				  return false;
+			  }
+		  }
+		  else {
+			  cout << "unknown include file " << filename << " at file " << in_file.string() << " at line " << line_number << endl;
+			  return false;
+		  }
+	  }
+	  else if (regex_match(line, match, ANGLE_RE)) {
+		  string filename = match[1];
+		  for (const auto& dir : include_directories) {
+			  if (filesystem::exists(dir / filename)) {
+				  included_path = dir / filename;
+				  found = true;
+				  break;
+			  }
+		  }
 
-			if (found) {
-				if (!PreprocessRecursive(included_path, out, include_directories)) {
-					return false;
-				}
-			}
-			else {
-				PrintErrorMessage(filename, in_file, line_number);
-				return false;
-			}
-		}
-		else {
-			out << line << "\n";
-		}
-	}
+		  if (found) {
+			  if (!PreprocessRecursive(included_path, out, include_directories)) {
+				  return false;
+			  }
+		  }
+		  else {
+			  cout << "unknown include file " << filename << " at file " << in_file.string() << " at line " << line_number << endl;
+			  return false;
+		  }
+	  }
+	  else {
+		  out << line << "\n";
+	  }
+  }
 	return true;
 }
 
-
-
 bool Preprocess(const path& in_file, const path& out_file, const vector<path>& include_directories) {
-	ifstream in(in_file);
-	if (!in.is_open()) return false;
-	in.close();
+  ifstream in(in_file);
+  if (!in.is_open()) return false;
+  in.close();
 
-	ofstream out(out_file);
-	if (!out.is_open()) return false;
+  ofstream out(out_file);
+  if (!out.is_open()) { 
+    return false; 
+  }
 
-	return PreprocessRecursive(in_file, out, include_directories);
+  return PreprocessRecursive(in_file, out, include_directories);
 }
 
 string GetFileContents(string file) {
-	ifstream stream(file);
+  ifstream stream(file);
 
-	return { (istreambuf_iterator<char>(stream)), istreambuf_iterator<char>() };
+  return { (istreambuf_iterator<char>(stream)), istreambuf_iterator<char>() };
 }
 
 void Test() {
-	error_code err;
-	filesystem::remove_all("sources"_p, err);
-	filesystem::create_directories("sources"_p / "include2"_p / "lib"_p, err);
-	filesystem::create_directories("sources"_p / "include1"_p, err);
-	filesystem::create_directories("sources"_p / "dir1"_p / "subdir"_p, err);
+  error_code err;
+  filesystem::remove_all("sources"_p, err);
+  filesystem::create_directories("sources"_p / "include2"_p / "lib"_p, err);
+  filesystem::create_directories("sources"_p / "include1"_p, err);
+  filesystem::create_directories("sources"_p / "dir1"_p / "subdir"_p, err);
 
-	{
-		ofstream file("sources/a.cpp");
-		file << "// this comment before include\n"
-			"#include \"dir1/b.h\"\n"
-			"// text between b.h and c.h\n"
-			"#include \"dir1/d.h\"\n"
-			"\n"
-			"int SayHello() {\n"
-			"    cout << \"hello, world!\" << endl;\n"
-			"#   include<dummy.txt>\n"
-			"}\n"s;
-	}
-	{
-		ofstream file("sources/dir1/b.h");
-		file << "// text from b.h before include\n"
-			"#include \"subdir/c.h\"\n"
-			"// text from b.h after include"s;
-	}
-	{
-		ofstream file("sources/dir1/subdir/c.h");
-		file << "// text from c.h before include\n"
-			"#include <std1.h>\n"
-			"// text from c.h after include\n"s;
-	}
-	{
-		ofstream file("sources/dir1/d.h");
-		file << "// text from d.h before include\n"
-			"#include \"lib/std2.h\"\n"
-			"// text from d.h after include\n"s;
-	}
-	{
-		ofstream file("sources/include1/std1.h");
-		file << "// std1\n"s;
-	}
-	{
-		ofstream file("sources/include2/lib/std2.h");
-		file << "// std2\n"s;
-	}
+  {
+	  ofstream file("sources/a.cpp");
+	  file << "// this comment before include\n"
+		  "#include \"dir1/b.h\"\n"
+		  "// text between b.h and c.h\n"
+		  "#include \"dir1/d.h\"\n"
+		  "\n"
+		  "int SayHello() {\n"
+		  "    cout << \"hello, world!\" << endl;\n"
+		  "#   include<dummy.txt>\n"
+		  "}\n"s;
+  }
+  {
+	  ofstream file("sources/dir1/b.h");
+	  file << "// text from b.h before include\n"
+		  "#include \"subdir/c.h\"\n"
+		  "// text from b.h after include"s;
+  }
+  {
+	  ofstream file("sources/dir1/subdir/c.h");
+	  file << "// text from c.h before include\n"
+		  "#include <std1.h>\n"
+		  "// text from c.h after include\n"s;
+  }
+  {
+	ofstream file("sources/dir1/d.h");
+	file << "// text from d.h before include\n"
+		"#include \"lib/std2.h\"\n"
+		"// text from d.h after include\n"s;
+  }
+  {
+	ofstream file("sources/include1/std1.h");
+	file << "// std1\n"s;
+  }
+  {
+    ofstream file("sources/include2/lib/std2.h");
+	file << "// std2\n"s;
+  }
 
 	assert((!Preprocess("sources"_p / "a.cpp"_p, "sources"_p / "a.in"_p,
 		{ "sources"_p / "include1"_p,"sources"_p / "include2"_p })));
@@ -174,5 +171,5 @@ void Test() {
 }
 
 int main() {
-	Test();
+  Test();
 }
